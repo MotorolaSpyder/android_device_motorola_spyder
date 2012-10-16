09b324b12d1aa4d071b43ed9c700ad98510e9c92
Raziel23x <raziel23x@apkmultitool.com>
2012-10-16 14:22:45 -0400

    cleanup

--- Android.mk
+++
@@ -1,3 +0,0 @@
-ifeq ($(TARGET_DEVICE),spyder)
-    include $(all-subdir-makefiles)
-endif
--- AndroidProducts.mk
+++
@@ -1,4 +0,0 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_spyder.mk
-ifeq ($(TARGET_PRODUCT),aokp_spyder)
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/aokp.mk
-endif
