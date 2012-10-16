073d7928de1afe458af8ac027c5eacbd679bdcaf
Raziel23x <raziel23x@apkmultitool.com>
2012-10-16 14:26:41 -0400

    trying to fix branch

---
+++ aokp.mk
@@ -0,0 +1,33 @@
+# Inherit AOSP device configuration for spyder.
+$(call inherit-product, device/motorola/spyder/full_spyder.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common_phone.mk)
+
+# Inherit GSM common stuff
+#$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# Common overlay
+DEVICE_PACKAGE_OVERLAYS += device/motorola/spyder/overlay/aokp
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_spyder
+PRODUCT_BRAND := verizon
+PRODUCT_DEVICE := spyder
+PRODUCT_DEVICE_PREFIX := cdma
+PRODUCT_MODEL := XT912
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_RELEASE_NAME := Droid RAZR
+PRODUCT_SFX := vzw
+
+DATE := $(shell date +%Y%m%d)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=${PRODUCT_DEVICE}_${PRODUCT_SFX} \
+    BUILD_FINGERPRINT=${PRODUCT_BRAND}/${PRODUCT_DEVICE}_${PRODUCT_SFX}/${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}:${PLATFORM_VERSION}/${BUILD_ID}/${DATE}:user/release-keys \
+    PRIVATE_BUILD_DESC="${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}-user ${PLATFORM_VERSION} ${BUILD_ID} ${DATE} release-keys" \
+
+# Copy spyder specific prebuilt files
+PRODUCT_COPY_FILES +=  \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_540_960.zip:system/media/bootanimation.zip
+
